49998ca7fecfbbe3ae112f18493b29ea01043ae8
marizoo <marizoo@meow.woof>
2022-01-15 00:06:16 +0700

    till tut 8

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,37 @@ import React from 'react'
 import { Routes, Route} from "react-router-dom";
 import Create from './pages/Create';
 import Notes from './pages/Notes';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { teal } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f8f8f8',
+    },
+    secondary: teal,
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+});
 
 const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Routes>
-        <Route  path="/" element={<Notes />}/>
-        <Route  path="create" element={<Create />}/>
+        <Route  path="/" element={<Create />}/>
+        <Route  path="create" element={<Notes />}/>
       </Routes>
-    </div>
+    </ThemeProvider>
   )
 }
 
 export default App
+
+//https://www.youtube.com/watch?v=TtJ3eCLYoRQ&list=PL4cUxeGkcC9gjxLvV4VEkZ6H6H4yWuS58&index=8
+// till video 8. radio button
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,14 +1,105 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { makeStyles } from '@mui/styles';
+import TextField from '@mui/material/TextField';
+
+
+const useStyles = makeStyles({
+   field: {
+       marginTop: 20,
+       marginBottom: 20,
+       display: 'block',
+   }
+})
 
 const Create = () => {
+
+    const classes = useStyles();
+    const [title, setTitle] = useState('');
+    const [details, setDetails] = useState('');
+    const [titleError, setTitleError] = useState(false);
+    const [detailsError, setDetailsError] = useState(false);
+    
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        setTitleError(false);
+        setDetailsError(false);
+
+        if(title === '') setTitleError(true);
+
+        if(details === '') setDetailsError(true);
+
+        if(title && details) {
+            console.log(title, details)
+        }
+
+
+    }
+
     return (
-        <div>
-            <Typography>
-                Create!
+        <Container>
+            <Typography 
+                variant="h6"
+                color= "textSecondary"
+                component="h2"
+                gutterBottom
+            >
+                Create a New Note
             </Typography>
-        </div>
+
+            <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+                <TextField 
+                    onChange={(ev)=> setTitle(ev.target.value)}
+                    value={title}
+                    className={classes.field}
+                    label='Note Title'
+                    variant='outlined'
+                    color='secondary'
+                    fullWidth
+                    required
+                    margin='normal'
+                    error={titleError}
+                />
+                <TextField 
+                    onChange={(ev)=> setDetails(ev.target.value)}
+                    value={details}
+                    className={classes.field}
+                    label='Details'
+                    variant='outlined'
+                    color='secondary'
+                    fullWidth
+                    required
+                    multiline
+                    rows={4}
+                    error={detailsError}
+                />
+            
+
+                <Button 
+                    type='submit'
+                    color='secondary'
+                    variant='contained'
+                    endIcon={<KeyboardArrowRightIcon />}
+                    margin='normal'
+                    >
+                    Submit
+                </Button>
+
+            </form>
+
+
+
+            {/* Icons */}
+            <br />
+           
+           
+           
+        </Container>
     )
 }
 
 export default Create
+
